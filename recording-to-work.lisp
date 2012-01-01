178e46990e1bf267956c0bbf7878400afebfb1c4
Rupert Swarbrick <rswarbrick@gmail.com>
2012-01-01 20:08:59 +0000

    Code to actually do the relating!

--- recording-to-work.lisp
+++ recording-to-work.lisp
@@ -41,3 +41,44 @@ in from DATE (which can be NIL)."
      :cookie-jar *cookie-jar*))
   (forget-cached recording)
   (forget-cached work))
+
+(defvar *r-to-w-queue* nil
+  "Fill this up before calling RELATE-RECORDINGS-TO-WORK")
+(defvar *r-to-w-work* nil
+  "Set this before calling RELATE-RECORDINGS-TO-WORK")
+
+(defun decide-about-recording (rec work)
+  (format t "Recording: ~A~%~A~%~%" rec (page rec))
+  (loop
+     (format t "Relate?~% (1) Yes~% (2) No~% ~
+                          (3) Inspect~% (*) No and quit~%   -> ")
+     (finish-output)
+     (let ((decision (read)))
+       (cond
+         ((eql decision 1)
+          (format t "Relating....")
+          (finish-output)
+          (relate-recording-to-work rec work :auto-edit t)
+          (format t " done.~%")
+          (return t))
+         ((eql decision 2)
+          (format t "Skipping.~%")
+          (return t))
+         ((eql decision 3)
+          (inspect rec))
+         (t
+          (format t "Ok, we're done!~%")
+          (return nil))))))
+
+(defun relate-recordings-to-work ()
+  (unless (typep *r-to-w-work* 'work)
+    (error "*R-TO-W-WORK* must be a work."))
+  (format t "Relating to the work: ~A~%~%" *r-to-w-work*)
+  (dolist (rec *r-to-w-queue*)
+    (unless (decide-about-recording rec *r-to-w-work*)
+      (return))
+    ;; Throw away the head of the queue, since we don't want to be asked about
+    ;; it again.
+    (pop *r-to-w-queue*))
+  (format t "~A recordings left in the list." (length *r-to-w-queue*))
+  (values))
